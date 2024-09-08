21de403205af887e85fdad9d3504548e2e2abded
Omar <omarkhaled1752003@yahoo.com>
2024-09-08 20:26:19 +0300

    fixed the method name

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from notifications.models import Notification, NotificationCount, FirebaseDeviceToken
 
-admin.site.add(Notification)
-admin.site.add(NotificationCount)
-admin.site.add(FirebaseDeviceToken)
+admin.site.register(Notification)
+admin.site.register(NotificationCount)
+admin.site.register(FirebaseDeviceToken)

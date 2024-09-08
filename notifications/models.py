b0cdc1d73f71995bd68f05bbaf490a2cab4c1bab
Omar <omarkhaled1752003@yahoo.com>
2024-09-08 18:52:44 +0300

    models added for Notifications, and device tokens

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Notification(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notifications"
+    )
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class NotificationCount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField(default=0)
+
+
+class FirebaseDeviceToken(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="device_tokens"
+    )
+    token = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
